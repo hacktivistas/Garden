63f35d74092474fdbf17fe945191a96514886da6
marimari <mari@colinaroja.org>
2013-04-05 08:09:29 +0200

    brankia_translate

--- locales/brankia_translate/definitions.php
+++ locales/brankia_translate/definitions.php
@@ -1,12 +1,12 @@
 <?php if (!defined('APPLICATION')) exit();
 
-$LocaleInfo['spanish_translate'] = array (
-  'Locale' => 'es-ES',
-  'Name' => 'spanish_translate',
-  'Description' => 'spanish_translate.',
+$LocaleInfo['brankia_translate'] = array (
+  'Locale' => 'es-ESbr',
+  'Name' => 'brankia_translate',
+  'Description' => 'brankia_translate.',
   'Version' => '1.22',
-  'Author' => 'Santiago Lozano',
-  'AuthorEmail' => 'santilozano@gmail.com',
+  'Author' => 'Santi',
+  'AuthorEmail' => 'santi@gmail.com',
   'AuthorUrl' => 'http://vanillaforums.org/profile/taik',
   'License' => 'GPL2',
   'Update by' => Konquet 
