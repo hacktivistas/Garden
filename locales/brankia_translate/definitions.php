61675458a8b437178a206123bb6e62441bbd1661
marimari <mari@colinaroja.org>
2013-04-05 08:05:25 +0200

    brankia_translate

---
+++ locales/brankia_translate/definitions.php
@@ -0,0 +1,15 @@
+<?php if (!defined('APPLICATION')) exit();
+
+$LocaleInfo['spanish_translate'] = array (
+  'Locale' => 'es-ES',
+  'Name' => 'spanish_translate',
+  'Description' => 'spanish_translate.',
+  'Version' => '1.22',
+  'Author' => 'Santiago Lozano',
+  'AuthorEmail' => 'santilozano@gmail.com',
+  'AuthorUrl' => 'http://vanillaforums.org/profile/taik',
+  'License' => 'GPL2',
+  'Update by' => Konquet 
+   
+   );
+
